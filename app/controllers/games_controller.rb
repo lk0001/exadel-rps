72e33b42928edb3ea5eb006c61318f4a8b68bd9a
Łukasz Kornek <lk0001@wp.pl>
2024-08-06 23:44:55 +0200

    Handle json requests

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,11 +3,23 @@ class GamesController < ApplicationController
   end
 
   def create
+    # TODO use external service instead of local fallack
     curb_choice = CurbService.new(nil).retrieve_throw
     user_choice = params[:choice]
 
     winner = RockPaperScissorsService.get_winner(user_choice, curb_choice)
 
-    redirect_to games_path, notice: "#{winner.humanize} (player's choice: #{user_choice}, Curb's choice: #{curb_choice}!"
+    respond_to do |format|
+      format.html {
+        redirect_to games_path, notice: "#{winner.humanize} (player's choice: #{user_choice}, Curb's choice: #{curb_choice}!"
+      }
+      format.json {
+        render json: {
+          curb_choice: curb_choice,
+          user_choice: user_choice,
+          winner: winner,
+        }
+      }
+    end
   end
 end
