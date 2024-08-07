2e83d9525a703ab01230e109d21e03fa4e6ea3b5
Łukasz Kornek <lk0001@wp.pl>
2024-08-07 14:30:12 +0200

    Rework choosing winner

--- app/services/rock_paper_scissors_service.rb
+++ app/services/rock_paper_scissors_service.rb
@@ -8,22 +8,21 @@ class RockPaperScissorsService
   PAPER = 'paper'.freeze
   SCISSORS = 'scissors'.freeze
 
-  CHOICES = [ROCK, PAPER, SCISSORS].freeze
-
-  # maybe rework RPS to be numbers
+  CHOICES = {
+    ROCK => 0,
+    PAPER => 1,
+    SCISSORS => 2,
+  }.freeze
 
   def self.get_winner(raw_user_choice, raw_curb_choice)
     user_choice = raw_user_choice.to_s.downcase
     curb_choice = raw_curb_choice.to_s.downcase
-    return ERROR unless CHOICES.include?(user_choice) && CHOICES.include?(curb_choice)
+    return ERROR unless CHOICES.keys.include?(user_choice) && CHOICES.keys.include?(curb_choice)
     return TIE if user_choice == curb_choice
-    case user_choice
-    when ROCK
-      curb_choice == PAPER ? CURB_WON : USER_WON
-    when PAPER
-      curb_choice == SCISSORS ? CURB_WON : USER_WON
-    when SCISSORS
-      curb_choice == ROCK ? CURB_WON : USER_WON
+    if CHOICES[user_choice] == (CHOICES[curb_choice] + 1) % 3
+      USER_WON
+    elsif CHOICES[curb_choice] == (CHOICES[user_choice] + 1) % 3
+      CURB_WON
     else
       ERROR
     end
